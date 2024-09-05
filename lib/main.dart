10008fece568dd0a5226556ebf8af96d9a785545
ph-value <phvalue123@gmail.com>
2024-09-05 20:21:29 +0900

    작은 화면 시 Drawer 적용 및 색상 변경

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 Future<void> signInAnonymously() async {
   try {
     await FirebaseAuth.instance.signInAnonymously();
@@ -15,7 +14,6 @@ Future<void> signInAnonymously() async {
 }
 
 Future<void> main() async {
-
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   // await signInAnonymously();
@@ -47,6 +45,7 @@ class CustomAppBarScreenTest extends StatefulWidget {
 
 class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
   int _selectedIndex = 0;
+  String _seletedMenu = "Posts";
 
   static final List<Widget> _widgetOptions = <Widget>[
     Page1(),
@@ -55,9 +54,10 @@ class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
     Page4(),
   ];
 
-  void _onItemTapped(int index) {
+  void _onItemTapped(int index, String menu) {
     setState(() {
       _selectedIndex = index;
+      _seletedMenu = menu;
     });
   }
 
@@ -69,14 +69,14 @@ class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
             children: <Widget>[
               Container(
                 alignment: Alignment.centerLeft,
-                color: Theme.of(context).primaryColor,
+                color: const Color(0xFF30383f),
                 height: height + 55,
                 width: MediaQuery.of(context).size.width,
                 // Background
                 child: const Padding(
                   padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
                   child: Text(
-                    "Home",
+                    "Sando`s Diary",
                     style: TextStyle(
                         fontSize: 25.0,
                         fontWeight: FontWeight.w600,
@@ -92,7 +92,7 @@ class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
                   left: 20.0,
                   right: 20.0,
                   child: Container(
-                    color: Theme.of(context).primaryColor,
+                    color: const Color(0xFF30383f),
                     height: height / 2,
                   )),
 
@@ -105,42 +105,64 @@ class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       GestureDetector(
-                        onTap: () => _onItemTapped(0),
+                        onTap: () => _onItemTapped(0, "Posts"),
                         child: Container(
-                          color: Colors.purple,
                           width: 80,
-                          height: _selectedIndex == 0 ? height + 40 : (height / 2) + 40,
-                          child: Center(child: Text("Posts")),
+                          height: _selectedIndex == 0
+                              ? height + 40
+                              : (height / 2) + 40,
+                          child: Center(
+                            child: Text("Posts"),
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF9581f5),
+                            border: Border.all(width: 1, color: Colors.black),
+                          ),
                         ),
                       ),
                       SizedBox(width: 10),
                       GestureDetector(
-                        onTap: () => _onItemTapped(1),
+                        onTap: () => _onItemTapped(1, "Projects"),
                         child: Container(
-                          color: Colors.orange,
                           width: 80,
-                          height: _selectedIndex == 1 ? height + 40 : (height / 2) + 40,
+                          height: _selectedIndex == 1
+                              ? height + 40
+                              : (height / 2) + 40,
                           child: Center(child: Text("Projects")),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF75F8AE),
+                            border: Border.all(width: 1, color: Colors.black),
+                          ),
                         ),
                       ),
                       SizedBox(width: 10),
                       GestureDetector(
-                        onTap: () => _onItemTapped(2),
+                        onTap: () => _onItemTapped(2, "About"),
                         child: Container(
-                          color: Colors.yellow,
                           width: 80,
-                          height: _selectedIndex == 2 ? height + 40 : (height / 2) + 40,
+                          height: _selectedIndex == 2
+                              ? height + 40
+                              : (height / 2) + 40,
                           child: Center(child: Text("About")),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFE1F970),
+                            border: Border.all(width: 1, color: Colors.black),
+                          ),
                         ),
                       ),
                       SizedBox(width: 10),
                       GestureDetector(
-                        onTap: () => _onItemTapped(3),
+                        onTap: () => _onItemTapped(3, "GuestBook"),
                         child: Container(
-                          color: Colors.green,
                           width: 80,
-                          height: _selectedIndex == 3 ? height + 40 : (height / 2) + 40,
+                          height: _selectedIndex == 3
+                              ? height + 40
+                              : (height / 2) + 40,
                           child: Center(child: Text("GuestBook")),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF99D6F9),
+                            border: Border.all(width: 1, color: Colors.black),
+                          ),
                         ),
                       ),
                     ],
@@ -150,11 +172,192 @@ class _CustomAppBarScreenState extends State<CustomAppBarScreenTest> {
         );
 
     return Scaffold(
-      appBar: _appBar(AppBar().preferredSize.height),
       // body: testBody(),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _widgetOptions,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          // 화면의 너비 정보를 가져옴
+          double width = constraints.maxWidth;
+
+          if (width > 600) {
+            // 큰 화면에서는 AppBar만 사용
+            return Scaffold(
+              appBar: _appBar(AppBar().preferredSize.height),
+              body: IndexedStack(
+                index: _selectedIndex,
+                children: _widgetOptions,
+              ),
+            );
+          } else {
+            // 작은 화면에서는 Drawer를 사용
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(_seletedMenu),
+              ),
+              endDrawer: Drawer(
+                backgroundColor: Colors.transparent,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 5,
+                          child: Container(
+                            color: const Color(0xFF30383f),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListView(
+                      padding: EdgeInsets.zero,
+                      children: <Widget>[
+                        Container(
+                          height: 180,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                          child: Center(
+                            child: Text(
+                              '   Sando`s Diary',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _onItemTapped(0, "Posts");
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF9581f5),
+                                border:
+                                    Border.all(width: 1, color: Colors.black),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: [
+                                    Flexible(flex: 1, child: Icon(Icons.list)),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Flexible(flex: 9, child: Text('Posts')),
+                                  ],
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _onItemTapped(1, "Projects");
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF75F8AE),
+                                border:
+                                    Border.all(width: 1, color: Colors.black),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: [
+                                    Flexible(
+                                        flex: 1,
+                                        child: Icon(Icons.book_outlined)),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Flexible(flex: 9, child: Text('Projects')),
+                                  ],
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _onItemTapped(2, "About");
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFE1F970),
+                                border:
+                                    Border.all(width: 1, color: Colors.black),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: [
+                                    Flexible(
+                                        flex: 1,
+                                        child: Icon(Icons.account_box_outlined)),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Flexible(flex: 9, child: Text('About')),
+                                  ],
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _onItemTapped(3, "GuestBook");
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF99D6F9),
+                                border:
+                                    Border.all(width: 1, color: Colors.black),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: [
+                                    Flexible(flex: 1, child: Icon(Icons.edit_outlined)),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Flexible(flex: 9, child: Text('GuestBook')),
+                                  ],
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              body: IndexedStack(
+                index: _selectedIndex,
+                children: _widgetOptions,
+              ),
+            );
+          }
+        },
+        // body: IndexedStack(
+        //   index: _selectedIndex,
+        //   children: _widgetOptions,
+        // ),
       ),
     );
   }
